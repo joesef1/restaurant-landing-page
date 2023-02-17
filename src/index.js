8de5624807ac16d7915c332cdab24716e49970e9
jousef1 <yousefel560@gmail.com>
2023-02-17 12:20:21 +0800

    edit

--- src/index.js
+++ src/index.js
@@ -6,3 +6,19 @@ import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+
+
+// # Restaurant Landing Page
+
+
+// I used React.js & pure CSS to build this Modern UI/UX website
+
+
+// ## Demo
+
+
+// https://joesef1.github.io/restaurant-landing-page/
+// ## Screenshots
+
+// ![App Screenshot](https://camo.githubusercontent.com/a85a3ea6d2267b7f09f0b5f7be8944e0fdd2eeae0cf80dd8706649e89678e936/68747470733a2f2f692e6962622e636f2f356a78424b70772f696d6167652e706e67)
+
